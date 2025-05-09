55f2bab4b86a116ebb7e089d3e89fa8a6c585b84
matheusmp3 <matheussparaiso11@gmail.com>
2025-05-09 20:03:49 -0300

    Salvando a página a página de login no github

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// isso é um comentario de linha
+
+/*
+isso é um comentario de bloco,
+ou seja,
+para mais de uma linha
+*/
+
+/* Definir o tipo de espaço de memória que eu desejo criar, podedendo ser:
+* "let" para variaveis -> O valor pode ser alterado no decorrer do algoritmo
+
+* "const" para constantes -> Não é possível alterar o valor armazenado neste tipo de dado.
+*/
+
+/*
+Após definir o tipo de dado, precisamos colocar qual será o nome da variável seguindo as regras apresentadas no slide.
+*/
+
+/* 
+Caso seja um dado do tipo let, poderá ser vazio.
+Caso seja um dado do tipo const, precisará ser informado o valor a ser armazenado na constante.
+*/
+
+
+
---
+++ script2.js
@@ -0,0 +1,8 @@
+function douglas() {
+    const nome = document.getElementById('marcos').value;
+    const idade = document.getElementById('gabriel').value;
+    alert(nome + ' possui ' + idade + ' anos ');
+    
+}
+
+
